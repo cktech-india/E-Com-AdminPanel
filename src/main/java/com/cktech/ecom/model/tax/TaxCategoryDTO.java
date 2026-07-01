1a89448415d53134866c068b243c6538bbe8f430
Athul <athulvineesh05@gmail.com>
2026-07-01 18:32:40 +0530

    updated tax category dto

--- src/main/java/com/cktech/ecom/model/tax/TaxCategoryDTO.java
+++ src/main/java/com/cktech/ecom/model/tax/TaxCategoryDTO.java
@@ -1,7 +1,13 @@
 package com.cktech.ecom.model.tax;
 
 import com.cktech.ecom.model.dto.Auditable;
-import jakarta.persistence.*;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Table;
+import jakarta.persistence.Id;
+import jakarta.persistence.GenerationType;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
