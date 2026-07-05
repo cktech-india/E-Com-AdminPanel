cb6c2bae43a633e41db16e700d7a91da024b9422
Athul <athulvineesh05@gmail.com>
2026-07-06 00:56:23 +0530

    Update ShippingChargeDTO.java

--- src/main/java/com/cktech/ecom/model/tax/ShippingChargeDTO.java
+++ src/main/java/com/cktech/ecom/model/tax/ShippingChargeDTO.java
@@ -6,7 +6,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.persistence.Column;
-import jakarta.persistence.GenerationType
+import jakarta.persistence.GenerationType;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
