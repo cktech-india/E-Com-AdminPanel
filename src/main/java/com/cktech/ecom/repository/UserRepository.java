ac0c48e8e816d245515e1939b977cc9f69bc3a84
Chempukutti <>
2026-07-15 12:23:29 +0530

    message

---
+++ src/main/java/com/cktech/ecom/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.cktech.ecom.repository;
+
+import com.cktech.ecom.model.user.UserDTO;
+import com.cktech.ecom.repository.common.GenericRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface UserRepository extends GenericRepository<UserDTO,Long> {
+
+}
